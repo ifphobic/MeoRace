e3ec614a0f8aafdcc3539353e182c42129afcf4d
phibi <phibi@phiDesk>
2014-06-05 19:01:53 +0200

    Reorg include

--- core/include.php
+++ core/include.php
@@ -1,12 +1,12 @@
 <?php
 
+   include("core/Configuration.php");
    include "core/database/AbstractDbFunction.php"; 
    include "core/database/CommonDbFunction.php";
    include("core/page/Role.php");
    include("core/page/CommonPageFunction.php");
    include("core/page/ModuleReader.php");
    include("core/page/ModulePage.php");
-   include("core/Configuration.php");
    include("core/page/Page.php");
    include("core/page/AbstractEditAction.php");
    include("core/interface/Module.php");
