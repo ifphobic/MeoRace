7eb6636b9330b0aba649d9bb36e0eef04c07d115
j-boss <jakoboss@MBP13-JB.local>
2014-06-12 00:04:50 +0200

    ajax tests, not working
    first tests with javascript getting a html page not working. possibly
    due to html and not a php snippet being the response…

---
+++ examplePages/racerdetail.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+
+    <div class="content_menu_spacer"></div>
+    <div class=racerdetail>
+        <div class=racerpic>
+            <img src="strom.jpg"/>
+        </div>
+        <div class=racer_number>666</div>
+        <div class=li_content>
+        <span class=li_firstline>Jerome "Strom" Thiriet</span><br>
+        <span class=li_secondline>Basel, Switzerland</span>
+        </div>
+    </div>
+    <div class=list>
+    <div class=listitem>
+        Auftrag 1
+    </div>
+    <div class=listitem>
+        Auftrag 2
+    </div>
+    <div class=listitem>
+        Auftrag 3
+    </div>
+    <div class=listitem>
+        Auftrag 4
+    </div>
+    </div>
