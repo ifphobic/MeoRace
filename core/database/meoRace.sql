ea6d0d58afe9b9c04a98e5503e67f1055e9e90ff
phibi <phibi@phiDesk>
2014-07-29 18:14:37 +0200

    Current DB Structure

--- core/database/meoRace.sql
+++ core/database/meoRace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 11, 2014 at 06:24 PM
+-- Generation Time: Jul 29, 2014 at 04:14 PM
 -- Server version: 5.1.70-log
 -- PHP Version: 5.5.12-pl0-gentoo
 
@@ -27,22 +27,27 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `Checkpoint`;
-CREATE TABLE IF NOT EXISTS `Checkpoint` (
+CREATE TABLE `Checkpoint` (
   `checkpointId` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(80) NOT NULL,
   `manned` tinyint(1) NOT NULL,
   `raceFk` int(11) NOT NULL,
   PRIMARY KEY (`checkpointId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `Checkpoint`
 --
 
 INSERT INTO `Checkpoint` (`checkpointId`, `name`, `manned`, `raceFk`) VALUES
-(1, 'Checkpoint 1', 1, 4),
-(2, 'Checkpoint2', 0, 4),
-(3, 'Checkpoint3', 0, 4);
+(1, 'Checkpoint1.1', 0, 4),
+(2, 'Checkpoint2', 1, 4),
+(3, 'Checkpoint3', 1, 4),
+(4, 'Checkpoint 4', 0, 4),
+(5, 'Checkpoint 5', 1, 4),
+(6, 'asgfasdf', 0, 4),
+(7, 'asdfasd', 0, 4),
+(8, 'asdfasdf', 0, 4);
 
 -- --------------------------------------------------------
 
@@ -51,23 +56,22 @@ INSERT INTO `Checkpoint` (`checkpointId`, `name`, `manned`, `raceFk`) VALUES
 --
 
 DROP TABLE IF EXISTS `Delivery`;
-CREATE TABLE IF NOT EXISTS `Delivery` (
+CREATE TABLE `Delivery` (
   `deliveryId` int(11) NOT NULL AUTO_INCREMENT,
   `taskFk` int(11) NOT NULL,
   `parcelFk` int(11) DEFAULT NULL,
   `pickupFk` int(11) NOT NULL,
   `dropoffFk` int(11) NOT NULL,
   PRIMARY KEY (`deliveryId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=28 ;
 
 --
 -- Dumping data for table `Delivery`
 --
 
 INSERT INTO `Delivery` (`deliveryId`, `taskFk`, `parcelFk`, `pickupFk`, `dropoffFk`) VALUES
-(16, 3, 4, 1, 2),
-(17, 3, 4, 2, 3),
-(18, 3, 4, 3, 1);
+(23, 3, NULL, 1, 1),
+(26, 3, NULL, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -76,20 +80,12 @@ INSERT INTO `Delivery` (`deliveryId`, `taskFk`, `parcelFk`, `pickupFk`, `dropoff
 --
 
 DROP TABLE IF EXISTS `DeliveryCondition`;
-CREATE TABLE IF NOT EXISTS `DeliveryCondition` (
+CREATE TABLE `DeliveryCondition` (
   `deliveryConditionId` int(11) NOT NULL AUTO_INCREMENT,
   `deliveryFk` int(11) NOT NULL,
   `previousDeliveryFk` int(11) NOT NULL,
   PRIMARY KEY (`deliveryConditionId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
-
---
--- Dumping data for table `DeliveryCondition`
---
-
-INSERT INTO `DeliveryCondition` (`deliveryConditionId`, `deliveryFk`, `previousDeliveryFk`) VALUES
-(22, 18, 17),
-(21, 17, 16);
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=57 ;
 
 -- --------------------------------------------------------
 
@@ -98,21 +94,24 @@ INSERT INTO `DeliveryCondition` (`deliveryConditionId`, `deliveryFk`, `previousD
 --
 
 DROP TABLE IF EXISTS `Parcel`;
-CREATE TABLE IF NOT EXISTS `Parcel` (
+CREATE TABLE `Parcel` (
   `parcelId` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(80) NOT NULL,
   `description` varchar(256) NOT NULL,
   `raceFk` int(11) NOT NULL,
   PRIMARY KEY (`parcelId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `Parcel`
 --
 
 INSERT INTO `Parcel` (`parcelId`, `name`, `description`, `raceFk`) VALUES
-(3, 'Parcel 2', 'for Race 1', 4),
-(4, 'Parcel1', 'for Race 1', 4);
+(3, 'Parcel 2', 'for Race 1cc', 4),
+(4, 'Parcel1', 'for Race 1', 4),
+(5, 'Parcel 3', 'for Race 1', 4),
+(6, 'asdfasdf', '', 4),
+(7, 'asdf', '', 4);
 
 -- --------------------------------------------------------
 
@@ -121,7 +120,7 @@ INSERT INTO `Parcel` (`parcelId`, `name`, `description`, `raceFk`) VALUES
 --
 
 DROP TABLE IF EXISTS `Race`;
-CREATE TABLE IF NOT EXISTS `Race` (
+CREATE TABLE `Race` (
   `raceId` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(256) NOT NULL,
   `status` enum('prepare','running','closed') NOT NULL,
@@ -133,7 +132,7 @@ CREATE TABLE IF NOT EXISTS `Race` (
 --
 
 INSERT INTO `Race` (`raceId`, `name`, `status`) VALUES
-(4, 'Race 1', 'prepare'),
+(4, 'Race 2', 'prepare'),
 (5, 'Race2', 'prepare');
 
 -- --------------------------------------------------------
@@ -143,7 +142,7 @@ INSERT INTO `Race` (`raceId`, `name`, `status`) VALUES
 --
 
 DROP TABLE IF EXISTS `Racer`;
-CREATE TABLE IF NOT EXISTS `Racer` (
+CREATE TABLE `Racer` (
   `racerId` int(11) NOT NULL AUTO_INCREMENT,
   `racerNumber` int(11) NOT NULL,
   `name` varchar(256) NOT NULL,
@@ -154,14 +153,16 @@ CREATE TABLE IF NOT EXISTS `Racer` (
   `status` enum('registered','active') NOT NULL,
   PRIMARY KEY (`racerId`),
   UNIQUE KEY `racerNumber` (`raceFk`,`racerNumber`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `Racer`
 --
 
 INSERT INTO `Racer` (`racerId`, `racerNumber`, `name`, `city`, `country`, `email`, `raceFk`, `status`) VALUES
-(1, 1, 'Racer 1', 'City', 'Country', 'Mail', 4, 'active');
+(1, 1, 'Racer 1', 'City', 'Country', 'Mail', 4, 'active'),
+(2, 0, 'name', 'city', 'country', 'mail', 4, 'active'),
+(3, 2, 'asdfasdf', '', '', '', 4, 'registered');
 
 -- --------------------------------------------------------
 
@@ -170,7 +171,7 @@ INSERT INTO `Racer` (`racerId`, `racerNumber`, `name`, `city`, `country`, `email
 --
 
 DROP TABLE IF EXISTS `Session`;
-CREATE TABLE IF NOT EXISTS `Session` (
+CREATE TABLE `Session` (
   `sessionId` varchar(42) NOT NULL,
   `userFk` int(11) NOT NULL,
   `loginTime` datetime NOT NULL,
@@ -207,7 +208,60 @@ INSERT INTO `Session` (`sessionId`, `userFk`, `loginTime`, `lastActive`) VALUES
 ('WlhN53frC6JVRRkh7n8SajJIwj7NSD6EUnAJ2abGF7', 7, '2014-06-08 17:38:29', '2014-06-08 17:38:29'),
 ('pTy5FkjMkUbZX9HQdFP1jabF685ZzZ6InCv2BN0kcz', 7, '2014-06-10 18:08:18', '2014-06-10 18:08:18'),
 ('ZicNej3OCXzHKJQn79tGHmHN2E6bEKkTeRYCQj9b4n', 7, '2014-06-11 18:24:15', '2014-06-11 18:24:15'),
-('Bd7GVoptUpTbpHs9t97WDEnc1xRglAnZkZEkYfgMKQ', 7, '2014-06-11 19:24:35', '2014-06-11 19:24:35');
+('Bd7GVoptUpTbpHs9t97WDEnc1xRglAnZkZEkYfgMKQ', 7, '2014-06-11 19:24:35', '2014-06-11 19:24:35'),
+('OfUsEe9sWBhNK1DMOX5a56sCHeRVrhk5QIOyRLRLyW', 7, '2014-06-14 18:18:53', '2014-06-14 18:18:53'),
+('gBAW38xjKdnsvHgfeQjM5BMt2dRGbjIc8OSXP0lcSG', 7, '2014-06-14 19:20:30', '2014-06-14 19:20:30'),
+('PBU6hDnwitBSbsG8932r7ExZWyN6B0Jes0vQPz2Phh', 7, '2014-06-14 19:20:59', '2014-06-14 19:20:59'),
+('15y3477st6rFv7riFEvORZxJvMk3dlGLQi7Y9nEFBy', 7, '2014-06-16 18:22:38', '2014-06-16 18:22:38'),
+('zjx1U9QfKkN3YYlnWIdSEUgRwUnwLZOM4bc0OpfIIC', 7, '2014-06-16 18:25:20', '2014-06-16 18:25:20'),
+('sz0WMmrjWaITkalU8N4mIBmP0mjupbExwJcrKa0Eyr', 7, '2014-06-16 18:41:05', '2014-06-16 18:41:05'),
+('ZPFAIQK5cOSXdILUiOhkbKbJM15l7qIgLsnQB9rZbl', 0, '2014-06-16 18:54:09', '2014-06-16 18:54:09'),
+('0pGAsSsVG9I5irJvGCO3ECkaVX2MWqSAkhYr0U29I9', 7, '2014-06-17 21:10:49', '2014-06-17 21:10:49'),
+('I7HfVPzQZgHfToh5izr1yczwT66m1c1I7sqJfInkJC', 7, '2014-06-17 21:13:32', '2014-06-17 21:13:32'),
+('sixBqaxzLwKUbC2PFORrFrvc0uL80b5d1QPTau4kDI', 7, '2014-06-17 21:15:37', '2014-06-17 21:15:37'),
+('GhJ792EHja7Y9Vfi4YJrQjByv41ZLrdBaTIJJFcNC6', 7, '2014-06-17 22:47:05', '2014-06-17 22:47:05'),
+('BZXTmkbCuhJBmaHdx3aXykmDePeZK3FplbWIRjsTT9', 7, '2014-06-18 20:32:33', '2014-06-18 20:32:33'),
+('zJgKUjbPjM9KVJEx3idmhg2hqNZDTiR32jrpoH5mi3', 7, '2014-06-18 21:34:16', '2014-06-18 21:34:16'),
+('pDSn96pcTvhIA2WH0MNN7f2ryKd4BZAsOngTYhU4Sr', 7, '2014-06-18 22:17:29', '2014-06-18 22:17:29'),
+('xsAbS1t8HGXXdQvnEKBkyTj2vwgNMi4ZXsLIDVKPV5', 7, '2014-06-18 23:19:54', '2014-06-18 23:19:54'),
+('3WfRO3qqHW2N0C8HNnvo9f1kVyYbNdu2UEQGEEXCHk', 7, '2014-06-18 23:35:39', '2014-06-18 23:35:39'),
+('QFNlhH9vsL0vA8wJRrDKWeJWKJJnxFoEwtI0cX0B40', 7, '2014-06-19 00:03:07', '2014-06-19 00:03:07'),
+('0jy8ovIcDhpR0bjTnbmrMwLnorMzFk2Bj9h3W5jnXV', 7, '2014-06-19 01:03:56', '2014-06-19 01:03:56'),
+('4oWNibUYCokLVUt2uqBAokpAxwD2p11Las7B6duMOw', 7, '2014-07-22 17:14:03', '2014-07-22 17:14:03'),
+('3eR1GDv2IczJfRADMDT7hk6FiCWPrApRfGx3Y3CnOt', 7, '2014-07-22 17:14:33', '2014-07-22 17:14:33'),
+('tNnN62gmn1R5Jf9lBGNcQzKmjgWS9J7Lyl7a5SzaRB', 0, '2014-07-22 17:23:52', '2014-07-22 17:23:52'),
+('1Gs4UHpNMMGTCyPVT8qiVBSFWvKBvb6RnqIe08zhxD', 11, '2014-07-22 17:28:14', '2014-07-22 17:28:14'),
+('ckLMaP0umu4xGR7ksLGwkNp3hfC7BkjzHZcMzUSCYe', 11, '2014-07-22 18:56:34', '2014-07-22 18:56:34'),
+('jJ4ZAHjHh14wL5oUkbGy5wDaxnMtCltzVvegRhcI37', 11, '2014-07-23 20:09:07', '2014-07-23 20:09:07'),
+('mwR3DU44xzlyqXsdqZfmW7eHusZF3V0nqNLp8vTBa5', 11, '2014-07-28 17:24:23', '2014-07-28 17:24:23'),
+('hPXCBJuxSaIVbi7NY9dgCNPiXGUxxCC81nkAQKgm0n', 11, '2014-07-28 18:32:05', '2014-07-28 18:32:05'),
+('eHotAkSVLstdXHe2msV3cPV8yayiHGhxIZQRzen0zZ', 11, '2014-07-28 19:36:23', '2014-07-28 19:36:23'),
+('os9GF0RecGOm1Zpf6EcyIKDPFX90Z4U7TTA8NfG4R8', 7, '2014-07-29 17:04:14', '2014-07-29 17:04:14'),
+('HxUTssKQFZtdBN5UzMqX6T5vwRz02r7lCRJ9RvcSTH', 7, '2014-07-29 18:12:35', '2014-07-29 18:12:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `StockExchangeDispatch`
+--
+
+DROP TABLE IF EXISTS `StockExchangeDispatch`;
+CREATE TABLE `StockExchangeDispatch` (
+  `taskFk` int(11) NOT NULL,
+  `raceFk` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `lastUpdate` datetime NOT NULL,
+  `counter` int(11) NOT NULL,
+  PRIMARY KEY (`taskFk`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `StockExchangeDispatch`
+--
+
+INSERT INTO `StockExchangeDispatch` (`taskFk`, `raceFk`, `price`, `lastUpdate`, `counter`) VALUES
+(3, 4, 25, '2014-07-28 18:19:21', 2),
+(6, 4, 13, '2014-07-28 18:02:57', 2);
 
 -- --------------------------------------------------------
 
@@ -216,22 +270,23 @@ INSERT INTO `Session` (`sessionId`, `userFk`, `loginTime`, `lastActive`) VALUES
 --
 
 DROP TABLE IF EXISTS `Task`;
-CREATE TABLE IF NOT EXISTS `Task` (
+CREATE TABLE `Task` (
   `taskId` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(80) NOT NULL,
   `maxDuration` int(11) NOT NULL COMMENT '[seconds]',
-  `currentPrice` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
   `description` varchar(256) NOT NULL,
   `raceFk` int(11) NOT NULL,
   PRIMARY KEY (`taskId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `Task`
 --
 
-INSERT INTO `Task` (`taskId`, `name`, `maxDuration`, `currentPrice`, `description`, `raceFk`) VALUES
-(3, 'Manifest1', 60, 100, 'Rundfahrt', 4);
+INSERT INTO `Task` (`taskId`, `name`, `maxDuration`, `price`, `description`, `raceFk`) VALUES
+(3, 'Manifest4dd', 602, 100, 'Rundfahrt', 4),
+(6, 'Manifest 2', 508, 50, 'Nop', 4);
 
 -- --------------------------------------------------------
 
@@ -240,7 +295,7 @@ INSERT INTO `Task` (`taskId`, `name`, `maxDuration`, `currentPrice`, `descriptio
 --
 
 DROP TABLE IF EXISTS `User`;
-CREATE TABLE IF NOT EXISTS `User` (
+CREATE TABLE `User` (
   `userId` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(80) NOT NULL,
   `password` varchar(80) NOT NULL,
@@ -248,7 +303,7 @@ CREATE TABLE IF NOT EXISTS `User` (
   `raceFk` int(11) NOT NULL,
   PRIMARY KEY (`userId`),
   UNIQUE KEY `user` (`user`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `User`
@@ -258,7 +313,8 @@ INSERT INTO `User` (`userId`, `user`, `password`, `role`, `raceFk`) VALUES
 (0, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin', -1),
 (7, 'master', 'fc613b4dfd6736a7bd268c8a0e74ed0d1c04a959f59dd74ef2874983fd443fc9', 'race master', 4),
 (8, 'registration', '29c9c30e0604515ced98b3d14fd88751a8f8e4b9bc69d483a67a257c14ab79fb', 'registration', 4),
-(10, 'master2', 'ee33bacb9843d3b4bac37e425b203d8aad4e08c6773d21a10533d66cd6bb7c02', 'race master', 5);
+(10, 'master2', 'ee33bacb9843d3b4bac37e425b203d8aad4e08c6773d21a10533d66cd6bb7c02', 'race master', 5),
+(11, 'dispatcher', 'ff7fce673d2f52b071129e34ff8ea570b7159527d019c81ec7756f4f8c821535', 'dispatcher', 4);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
