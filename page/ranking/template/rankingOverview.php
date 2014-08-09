5514bbd3bd3814a3b04086c73207467b9048dd68
phibi <phibi@phiDesk>
2014-08-09 21:03:22 +0200

    Ranking overview with dynamic data

--- page/ranking/template/rankingOverview.php
+++ page/ranking/template/rankingOverview.php
@@ -1,40 +1,44 @@
 <div class="content_tab" id="rankingoverview_ranking">
-  
-    <div class="bottom_content_wrapper">
+<div class="bottom_content_wrapper">
+   <ul>
 
-<ul>
 <?php
    
-   $dbFunction = new RacerDbFunction();
-   $racers = $dbFunction->findAll( CommonDbFunction::getUser()->raceFk );
+   include 'page/ranking/RankingCalculator.php';
+
+   $dbFunction = new RankingDbFunction();
+   $racerTasks = $dbFunction->findAll( CommonDbFunction::getUser()->raceFk );
    $dbFunction->close();
 
-   print("<div class='bottom_content_wrapper'><ul>");
-   foreach ( $racers as $racer ) {
+   $rankings = RankingCalculator::evaluate( $racerTasks );
+   
+
+   foreach ( $rankings as $ranking ) {
       print("
-         <li onclick='" . Page::getOnClickFunction( "racerTask", "racerTask", $racer->racerId ) . "'>
+         <li onclick='" . Page::getOnClickFunction( "ranking", "rankingDetail", $ranking->racerId ) . "'>
             <div class='left_info'>
-               <img src='" . Page::getImagePath( $racer ) . "' alt='strom praesi'>
+               <img src='" . Page::getImagePath( $ranking ) . "'>
             </div> 
             <div class='middle_info'>
-               <p class='title'>" . $racer->name . "</p>
-               <p class='description'>"  . $racer->city . ", " . $racer->country . "</p>
+               <p class='title'>" . $ranking->name . "</p>
+               <p class='description'>"  . $ranking->city . ", " . $ranking->country . "</p>
             </div>
             
-            // Needs dynamic code
             <div class='middle_info racer_ranking_points'>
-      <p> <span class='racer_ranking'>1</span> <span class='racer_points'>1700</span> </p>
-      </div>
-            
-            
+               <p>
+                  <span class='racer_ranking'>" . $ranking->ranking . "</span>
+                  <span class='racer_points'>" . $ranking->score . "</span>
+               </p>
+            </div>
             <div class='right_info'>
-               <p class='rider_number'>" . $racer->racerNumber . "</p>
+               <p class='rider_number'>" . $ranking->racerNumber . "</p>
             </div>
          </li>
       ");
    }
-   print("</ul></div>");
 
 ?>
 
+   </ul>
+</div>
 </div>
