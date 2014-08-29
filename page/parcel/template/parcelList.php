d6e8cf56e08b3ab5a690cfa90ee461b639f3a128
phibi <phibi@phiNote>
2014-08-29 19:25:51 +0200

    Small fix

--- page/parcel/template/parcelList.php
+++ page/parcel/template/parcelList.php
@@ -1,18 +1,11 @@
 <div class="content_tab" id="parcelList">
-<<<<<<< HEAD
-=======
     <div class="bottom_content_wrapper">
       <div class="bottom_info_wrapper">
->>>>>>> FETCH_HEAD
          
 <div class='new_button' onclick='<?php print(Page::getOnClickFunction( "parcel", "parcelEdit", null, "New Parcel" ) ) ?> '>+ Add Parcel</div>
 
 <ul>
-<<<<<<< HEAD
-   <?php
-=======
 <?php
->>>>>>> FETCH_HEAD
       
       $dbFunction = new ParcelDbFunction();
       $parcels = $dbFunction->findAll( CommonDbFunction::getUser()->raceFk );
@@ -20,11 +13,7 @@
 
       foreach ( $parcels as $parcel ) {
       print ("
-<<<<<<< HEAD
       	<li class='riderlist' onclick='" . Page::getOnClickFunction( "parcel", "parcelEdit", $parcel->parcelId, "edit") . "'>
-=======
-         <li class='riderlist' onclick='" . Page::getOnClickFunction( "parcel", "parcelEdit", $parcel->parcelId, "edit") . "'>
->>>>>>> FETCH_HEAD
             <div class='listwrapper'>
             <div class='left_info'><img src='" . Page::getImagePath( $parcel ) . "' height=65px width=65px;>
                </div> 
@@ -40,12 +29,7 @@
       ");
       }
 
-<<<<<<< HEAD
    ?>
 </ul>
 </div>
 
-=======
-?> 
-</ul> 
->>>>>>> FETCH_HEAD
