740e5c2be048b338201050f56c8d9d636d35acbc
phibi <phibi@phiDesk>
2015-07-15 22:50:48 +0200

    db script korrektur

--- core/database/update.2.sql
+++ core/database/update.2.sql
@@ -1,2 +1,3 @@
 
-CREATE TABLE `meoRace`.`UserRace` ( `userRaceId` INT NOT NULL AUTO_INCREMENT , `userFk` INT NOT NULL , `raceFk` INT NOT NULL , PRIMARY KEY (`userRaceId`) ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+CREATE TABLE `UserRace` ( `userRaceId` INT NOT NULL AUTO_INCREMENT , `userFk` INT NOT NULL , `raceFk` INT NOT NULL , PRIMARY KEY (`userRaceId`) ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+INSERT INTO UserRace (userFk , raceFk) (select userId, raceFk from User where role = 'race master');
